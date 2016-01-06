76cd3d9783feb65dbb00e3607006cabfa32a1d5a
simonchox <simoncellisantiago@gmail.com>
2016-01-06 20:53:11 -0300

    arreglo de footer y contacto

--- core/views.py
+++ core/views.py
@@ -24,28 +24,10 @@ class ContactFormView(FormView):
             subject=form.cleaned_data.get('subject').strip(),
             message=message,
             from_email='contacto.gigosoft@myapp.com',
-            recipient_list=['contacto.gigosoft@gmail.com'],
+            recipient_list=['simoncellisantiago@gmail.com'],
         )
         return super(ContactFormView, self).form_valid(form)
 
 def contact(request):
     contact_form = ContactFormView()
     return render(request, 'Contacto.html', {'contact_form': contact_form})
-
-#def contacto(request):
-#    if request.method=='POST':
-#        form = ContactForm(request.POST)
-#        if form.is_valid():
-#            subject = form.cleaned_data['subject']
-#            message = form.cleaned_data['message']
-#            sender = form.cleaned_data['sender']
-#            cc_myself = form.cleaned_data['cc_myself']
-#            recipients = ['info@example.com']
-#            if cc_myself:
-#                recipients.append(sender)
-#            send_mail(subject, message, sender, recipients)
-#            return HttpResponseRedirect('/')
-#    else:
-#        formulario = ContactForm()
-#    return render(request, 'Contacto.html', {'formulario':formulario})
-
